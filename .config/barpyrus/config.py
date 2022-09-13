76304ea04af829466993ce3829bb934d8e7e36eb
Nekit <33904988+i-just-lizard@users.noreply.github.com>
2022-09-13 22:54:07 +0300

    Add files via upload

--- .config/barpyrus/config.py
+++ .config/barpyrus/config.py
@@ -4,57 +4,17 @@ from barpyrus.core import Theme
 from barpyrus import lemonbar
 from barpyrus import conky
 import sys
-# Copy this config to ~/.config/barpyrus/config.py
 
+hc = hlwm.connect()
 # set up a connection to herbstluftwm in order to get events
 # and in order to call herbstclient commands
-hc = hlwm.connect()
-
 # get the geometry of the monitor
 monitor = sys.argv[1] if len(sys.argv) >= 2 else 0
 (x, y, monitor_w, monitor_h) = hc.monitor_rect(monitor)
-height = 28 # height of the panel
+height = 16 # height of the panel
 width = monitor_w # width of the panel
 hc(['pad', str(monitor), str(height)]) # get space for the panel
 
-# An example conky-section:
-# icons
-bat_icons = [
-    0xe242, 0xe243, 0xe244, 0xe245, 0xe246,
-    0xe247, 0xe248, 0xe249, 0xe24a, 0xe24b,
-]
-# first icon: 0 percent
-# last icon: 100 percent
-bat_delta = 100 / len(bat_icons)
-conky_text = '%{F\\#D8DEE9}%{T2}\ue026%{T-}%{F\\#989898}${cpu}% '
-conky_text += '%{F\\#D8DEE9}%{T2}\ue021%{T-}%{F\\#989898}${memperc}% '
-#conky_text += '%{F\\#D8DEE9}%{T2}\ue13c%{T-}%{F\\#989898}${downspeedf}K '
-#conky_text += '%{F\\#D8DEE9}%{T2}\ue13b%{T-}%{F\\#989898}${upspeedf}K '
-conky_text += "${if_existing /sys/class/power_supply/BAT1}"
-conky_text += "%{T2}"
-conky_text += "${if_match \"$battery\" == \"discharging ${battery_percent BAT1}%\"}"
-conky_text += "%{F\\#EBCB8B}"
-conky_text += "$else"
-conky_text += "%{F\\#D8DEE9}"
-conky_text += "$endif"
-for i,icon in enumerate(bat_icons[:-1]):
-    conky_text += "${if_match ${battery_percent BAT1} < %d}" % ((i+1)*bat_delta)
-    conky_text += chr(icon)
-    conky_text += "${else}"
-conky_text += chr(bat_icons[-1]) # icon for 100 percent
-for _ in bat_icons[:-1]:
-    conky_text += "${endif}"
-conky_text += "%{T-} ${battery_percent BAT1}% "
-conky_text += "${endif}"
-conky_text += "%{F-}"
-
-# example options for the hlwm.HLWMLayoutSwitcher widget
-xkblayouts = [
-    'us us -variant altgr-intl us'.split(' '),
-    'de de de'.split(' '),
-]
-setxkbmap = 'setxkbmap -option compose:menu -option ctrl:nocaps'
-setxkbmap += ' -option compose:ralt -option compose:rctrl'
 
 # you can define custom themes
 grey_frame = Theme(bg = '#303030', fg = '#EFEFEF', padding = (3,3))
@@ -62,25 +22,10 @@ grey_frame = Theme(bg = '#303030', fg = '#EFEFEF', padding = (3,3))
 # Widget configuration:
 bar = lemonbar.Lemonbar(geometry = (x,y,width,height))
 bar.widget = W.ListLayout([
-    W.RawLabel('%{l}'),
-    hlwm.HLWMTags(hc, monitor, tag_renderer = hlwm.underlined_tags),
-    W.RawLabel('%{c}'),
-    hlwm.HLWMMonitorFocusLayout(hc, monitor,
-           # this widget is shown on the focused monitor:
-           grey_frame(hlwm.HLWMWindowTitle(hc)),
-           # this widget is shown on all unfocused monitors:
-           conky.ConkyWidget('df /: ${fs_used_perc /}%')
-                                    ),
-    W.RawLabel('%{r}'),
-    conky.ConkyWidget(text= conky_text),
-    # something like a tabbed widget with the tab labels '>' and '<'
-    W.ShortLongLayout(
-        W.RawLabel(''),
-        W.ListLayout([
-           # hlwm.HLWMLayoutSwitcher(hc, xkblayouts, command = setxkbmap.split(' ')),
-            W.RawLabel(' '),
-        ])),
-        grey_frame(W.DateTime('%d. %B, %H:%M')),
-])
-
+        W.RawLabel('%{l}'),
+            hlwm.HLWMTags(hc, monitor),
+                        W.RawLabel('%{r}'),
+                        conky.ConkyWidget('battery: ${battery_percent BAT1}% '),
+                        grey_frame(W.DateTime('%d. %B, %H:%M')),
+                                ])
 
